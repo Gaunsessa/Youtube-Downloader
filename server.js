729b4921d595df3cfa6948f33dff718ee8aa15a7
Gauns <43172308+Gauns@users.noreply.github.com>
2019-10-15 18:19:42 +1100

    Set up basic express site

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 var youtubedl = require('youtube-dl');
+
+const PORT = 80;
+
+app.get('/', function(req, res) {
+    res.send('yeet227');
+});
+
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
