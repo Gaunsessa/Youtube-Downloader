654825316ee8248b0bba96204390f9fd7272b1a4
Gauns <43172308+Gauns@users.noreply.github.com>
2019-10-15 19:37:25 +1100

    Finished Basic Video Downloader

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
-var express = require('express');
-var app = express();
-var youtubedl = require('youtube-dl');
+const express = require('express');
+const youtubedl = require('youtube-dl');
+const path = require('path');
+const fs = require('fs');
 
+const app = express();
 const PORT = 80;
+const WORDS = JSON.parse(fs.readFileSync('words.json'))
 
-app.get('/', function(req, res) {
-    res.send('yeet227');
+app.use(express.static('videos'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+app.get('/post', (req, res) => {
+    if ('url' in req.query) {
+        var video = youtubedl(req.query['url']);
+        var name = getFileName();
+        video.pipe(fs.createWriteStream(path.join('videos/' + name + '.mp4')));
+        res.send(`<script>window.open('/${name}.mp4', '_self')</script>`);
+    }
+});
+
+function getFileName() {
+    var adj = WORDS['adjectives'][Math.floor(Math.random() * Object.keys(WORDS['adjectives']).length)];
+    var verb = WORDS['nouns'][Math.floor(Math.random() * Object.keys(WORDS['nouns']).length)];
+    var name = adj + verb;
+    console.log(fs.readdirSync('videos'));
+    if (`${name}.mp4` in fs.readdirSync('videos')) {
+        return getFileName();
+    }
+    return name;
+}
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
