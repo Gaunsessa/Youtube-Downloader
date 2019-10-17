f9f6b1e18b63ef495a064041e291e5fbdddbb28c
Gauns <43172308+Gauns@users.noreply.github.com>
2019-10-17 13:29:41 +1100

    Stream line

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = 1273;
 const WORDS = JSON.parse(fs.readFileSync('words.json'))
 
 app.use(express.static('videos'));
-app.use(express.static('imgs'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
